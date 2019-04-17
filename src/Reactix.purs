96731bfd1463d2db21dfa348d4d50ce6c5b89c54
James Laver <james.laver@gmail.com>
2019-04-17 10:55:30 +0200

    Remove Reactix.Mount, render will move to Reactix.React

--- src/Reactix.purs
+++ src/Reactix.purs
@@ -1,9 +1,7 @@
 module Reactix
   ( module Reactix.React
   , module Reactix.Hooks
-  , module Reactix.Mount
   ) where
 
 import Reactix.React
 import Reactix.Hooks
-import Reactix.Mount (findElementBySelector, render)
--- src/Reactix/Mount.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var react_dom = require('react-dom');
-
-exports._getElementBySelector = function(sel) {
-  return window.top.document.getElementBySelector(sel);
-};
-
-exports._render = function(a,b) { return react_dom.render(a,b); };
-
