02645d68a58e3ed99c3369860b0349e09d8243a2
James Laver <james.laver@gmail.com>
2019-06-23 17:56:42 +0200

    Only export SyntheticEvent from SyntheticEvent in Reactix

--- src/Reactix.purs
+++ src/Reactix.purs
@@ -1,7 +1,9 @@
 module Reactix
   ( module Reactix.React
   , module Reactix.Hooks
+  , module Reactix.SyntheticEvent
   ) where
 
 import Reactix.React
 import Reactix.Hooks
+import Reactix.SyntheticEvent (SyntheticEvent)
